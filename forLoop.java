2cf3f7168b18ba630d6b85758e1fd82213e240a0
cagdaskalak <64699562+cagdaskalak@users.noreply.github.com>
2021-10-12 12:11:38 +0300

    Add files via upload

---
+++ forLoop.java
@@ -0,0 +1,25 @@
+public class forLoop {
+    public static void main(String[] args) throws Exception {
+        
+        // for loop = executes a block of code a limited amount of times
+
+
+    for(int i=10; i>=0; i--){
+        System.out.println(i);
+    }
+
+    System.out.println("Happy new year!");
+
+        //     for(int i=0; i>=10; i++){
+        // System.out.println(i);               // 0 dan başlayarak 10 a kadar gider.
+       //  }
+
+       //     for(int i=10; i>=0; i-=2){
+        // System.out.println(i);               // 2 şer azaltarak 0 a gider
+       //  }
+
+        //     for(int i=0; i>=10; i+=2){
+        // System.out.println(i);               // 2 şer arttırarak 10 a gider
+       //  }
+    }
+}
